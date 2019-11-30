0cf2103190450ca10228901a80ee2a5ab4eda934
Talon120 <taloncjones@gmail.com>
2019-11-29 21:07:53 -0800

    Pass function to square

--- src/index.js
+++ src/index.js
@@ -3,18 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: null,
-    }
-  }
-
   render() {
     return (
-      <button className="square"
-        onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
       </button>
     );
   }
@@ -27,8 +22,12 @@ class Board extends React.Component {
       squares: Array(9).fill(null)
     }
   }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)} />
+    );
   }
 
   render() {
