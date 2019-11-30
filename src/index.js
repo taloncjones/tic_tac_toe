427ecadd60c17ce7adee4d37815450ea488f5162
Talon120 <taloncjones@gmail.com>
2019-11-29 21:48:08 -0800

    Ignore clicks if winner or occupied square

--- src/index.js
+++ src/index.js
@@ -25,7 +25,10 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice()
-    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({ squares: squares, xIsNext: !this.state.xIsNext })
   }
 
