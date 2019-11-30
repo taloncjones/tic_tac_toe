69189e8558b09c9617eb861795d9510a6f1ed5bd
Talon120 <taloncjones@gmail.com>
2019-11-29 22:17:51 -0800

    Show game status

--- src/index.js
+++ src/index.js
@@ -113,7 +113,7 @@ class Game extends React.Component {
           />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
